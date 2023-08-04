02af47b25dcc4be71f1123fe7636694b2c0d988d
forte97 <cruzforte97@scls.anonaddy.com>
2023-08-05 07:26:05 +0800

    first upload

---
+++ scripts/symlinks.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+ln -sf ../dotfiles/.bash_profile ~/.bash_profile
+ln -sf ../dotfiles/.bashrc ~/.bashrc
+ln -sf ../dotfiles/.xbindkeysrc ~/.xbindkeysrc
+ln -sf ../dotfiles/.xinitrc ~/.xinitrc
+ln -sf ../dotfiles/.config/picom/picom.conf ~/.config/picom/picom.conf
